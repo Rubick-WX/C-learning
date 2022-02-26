b8247e4d0a2a290280f03313a41b0a4128cf9f0f
WX_r7000p <ruabick@qq.com>
2022-02-26 12:12:13 +0800

    main

--- C++程序设计/运算符重载-4-大整数的加减乘除.cpp
+++ C++程序设计/运算符重载-4-大整数的加减乘除.cpp
@@ -1,29 +1,53 @@
-#include <iostream>
+#include<iostream>
 #include <cstring>
 using namespace std;
 // 在此处补充你的代码
 
-class pInt{
-    char *s;
+class pInt
+{
+    char *c;
+    int *n;
 
-    public:
-        friend istream &operator>>(istream &a, pInt &b);
+public:
+    friend istream &operator>>(istream &a, pInt &b);
 
     //构造与析构
-
+    pInt()
+    {
+        c = new char[100];
+        n = new int[100];
+    }
+    ~pInt()
+    {
+        delete[] c;
+        delete[] n;
+    }
     //符号重载
 };
 
-istream & operator>>(istream &a,pInt &b){
+istream &operator>>(istream &a, pInt &b)
+{
+    cin>>b.c;
+
+    return a;
+}
+istream &operator>>(istream &a, char b)
+{
 
     return a;
 }
 
-int main(){
+int main()
+{
     pInt a, b;
     char o;
     cin >> a >> o >> b;
-
-
+    switch(o){
+    case '+':;
+    case '-':;
+    case '*':;
+    case '/':;
+    
+    }
     return 0;
 }
